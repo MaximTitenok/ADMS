8398d2815802ce7b97f59a53a55c3521e5213edc
MaximTitenok <maximtiten@gmail.com>
2023-08-13 14:09:11 +0300

    Add project files.

---
+++ ADMS/BD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADMS
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        //public DbSet<TransactionModel> Transactions { get; set; }
+        //public DbSet<UserModel> Users { get; set; }
+    }
+}
